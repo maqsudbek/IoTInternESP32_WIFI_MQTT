b846b2b59666ca94a0739444a04b0ccd526651de
OTABEKDAVRON <otabekdavr2003@gmail.com>
2024-04-25 11:34:06 +0500

    wifi setup, mqtt setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,95 @@
-
-#include <Arduino.h>
-
 // Include necessary libraries here
-// TODO: Add necessary libraries here
+#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 
 // Define global variables here
-// TODO: Add global variables here
+#define lcdAddress 0x27 // link to the address number of LCD
+#define LED_R 32
+#define LED_G 25
+#define LED_Y 33
+#define BUTTON 26
+
+const char* ssid = "IoTLaB";
+const char* password = "iot303abab";
+const char* mqtt_server = "mqtt.iotserver.uz";
+const int mqtt_port = 1883;
+const char* mqtt_username = "userTTPU";
+const char* mqtt_password = "mqttpass";
+const char* myID = "OtabekDavronbek";
+
+unsigned long lastReconnectAttempt = 0;
+const unsigned long reconnectInterval = 10000;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 // Define functions here
-// TODO: Add functions here
+    // Function to set up WiFi connection
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+    //Function to set up MQTT connection 
+void setup_mqtt(){
+  client.setServer(mqtt_server, mqtt_port);
+}
+    //Function to reconnect to MQTT
+void reconnect(){
+  if(client.connected()){
+    return;
+  }
+
+  if(millis() - lastReconnectAttempt > reconnectInterval) {
+    lastReconnectAttempt = millis();
+    Serial.print("Attempting MQTT connection...");
+    if(client.connect(myID, mqtt_username, mqtt_password)){
+      Serial.println("connected");
+ //     client.publish("ttpu/");
+ //     client.subscribe("");
+    }
+    else{
+      Serial.print("failure, rc = ");
+      Serial.print(client.state());
+      Serial.println("try again in  seconds");
+    }
+  }
+}
 /**********************************/
 // SETUP //
 void setup(void) 
 {
+  Serial.begin(115200);
+
   // Initialize peripherals
-  // TODO: Initialize peripherals here
-  // TODO: LEDS, Buttons, LCD, etc.
+  LiquidCrystal_I2C lcd(lcdAddress, 16, 2); // initializing LCD with the address, columns, rows
+  Wire.begin();
+  lcd.begin(16, 2);
+  lcd.backlight(); // turn backlight on
+  lcd.setCursor(3, 0); 
+  lcd.print("WAY AHEAD!");
 
   // Connect to WiFi
-  // TODO: Connect to WiFi here
+  setup_wifi();
 
   // Setup MQTT
-  // TODO: Setup MQTT here
-
-  // 
-
+  setup_mqtt();
 }
 
 /**********************************/
@@ -33,5 +97,11 @@ void setup(void)
 void loop(void) 
 {
   // Keep WiFi and MQTT connection alive
-  // TODO: Keep WiFi and MQTT connection alive here
+  if(WiFi.status() != WL_CONNECTED){
+    setup_wifi();
+  }
+  if(!client.connected()){
+    reconnect();
+  }
+  client.loop();
 }
